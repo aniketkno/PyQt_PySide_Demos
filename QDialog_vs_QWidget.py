69cd706f53bed47921a90ff1559e3edda8cc198c
aniketkno <3714725+aniketkno@users.noreply.github.com>
2021-04-24 09:31:50 -0600

    Add layout and buttons

--- QDialog_vs_QWidget.py
+++ QDialog_vs_QWidget.py
@@ -18,7 +18,23 @@ class TestDialog(QtWidgets.QDialog):
         self.setWindowTitle("Test Dialog")
         self.setWindowFlags(self.windowFlags() ^ QtCore.Qt.WindowContextHelpButtonHint)
         self.setMinimumWidth(200)
+        self.create_widgets()
+        self.create_layouts()
 
+    def create_widgets(self):
+        self.lineedit = QtWidgets.QLineEdit()
+        self.checkbox1 = QtWidgets.QCheckBox("CheckBox1")
+        self.checkbox2 = QtWidgets.QCheckBox("CheckBox2")
+        self.button1 = QtWidgets.QPushButton("Button1")
+        self.button2 = QtWidgets.QPushButton("Button2")
+
+    def create_layouts(self):
+        main_layout = QtWidgets.QVBoxLayout(self)
+        main_layout.addWidget(self.lineedit)
+        main_layout.addWidget(self.checkbox1)
+        main_layout.addWidget(self.checkbox2)
+        main_layout.addWidget(self.button1)
+        main_layout.addWidget(self.button2)
 
 if __name__ == "__main__":
     d = TestDialog()
